dbd425af24f146ed3dacc9ec082a3211e7d12687
Igor Roberto <roberto.igor.18@gmail.com>
2025-07-15 00:49:59 -0300

    New contact info in about me page

--- src/pages/AboutMe/index.jsx
+++ src/pages/AboutMe/index.jsx
@@ -12,6 +12,11 @@ const ProfileDescStyled = styled.section`
     p {
         font-size: 18px
     }
+
+    .link-social-media {
+        color: white;
+        text-decoration: underline;
+    }
 `
 
 const ImgProfileStyled = styled.img`
@@ -29,6 +34,13 @@ const AboutMe = () => {
                     <p>I'm a <strong>Front-End Developer</strong> with a strong foundation in <strong>JavaScript</strong> and experience working with frameworks like <strong>React</strong>.</p>
                     <p>I also have a background in <strong>Software Quality</strong>, which has given me a complete perspective on the software development lifecycle. From code to user experience.</p>
                     <p>I'm currently improving my skills in <strong>TypeScript</strong> and <strong>Node.js</strong> to better my full-stack capabilities.</p>
+                    <hr></hr>
+                    <h3>How to find me</h3>
+                    <ul>
+                        <li>LinkedIn: <a className="link-social-media" href="https://www.linkedin.com/in/igor-roberto-santos/" target="_blank">linkedin.com/igor-roberto-santos</a></li>
+                        <li>E-mail: <a className="link-social-media" href="mailto: roberto.igor.18@gmail.com" target="_blank">roberto.igor.18@gmail.com</a></li>
+                        <li>GitHub: <a className="link-social-media" href="https://github.com/ir2508" target="_blank">ir2508</a></li>
+                    </ul>
                 </TextLabel>
             </ProfileDescStyled>
             <section></section>
